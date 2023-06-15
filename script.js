742e2b9b75429e9b07a3e97b7e28cc7ee0c5b4d1
vshirmanov <95222817+vshirmanov@users.noreply.github.com>
2023-06-15 18:53:22 +0300

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ async function checkWeather(city) {
         document.querySelector(".error").style.display = "block";
         document.querySelector(".weather").style.display = "none";
     } else {
-        var data = await response.json();
+        const data = await response.json();
         document.querySelector(".weather").style.display = "block";
         document.querySelector(".city").innerHTML = data.name;
         document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
